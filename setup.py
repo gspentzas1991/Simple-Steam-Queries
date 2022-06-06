6e6c936010ae49440b912fe8503b5e9059de8dea
Giannis Spentzas <gspentzas1991@gmail.com>
2022-06-06 06:03:35 +0300

    finished the pip upload

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Simple-Steam-Queries",
+    name="simple_steam_queries",
     install_requires=['steam'],
-    version="1.0.0",
+    version="1.0.4",
     author="Giannis Spentzas",
     author_email="gspentzas1991@gmail.com",
     description="A python module that allows you to easily run Steam Master Server Query Protocol queries",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gspentzas1991/Simple-Steam-Queries",
-    packages=find_packages(include=['Simple-Steam-Queries', 'Simple-Steam-Queries.*']),
+    packages=find_packages(include=['simple_steam_queries', 'simple_steam_queries.*']),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
---
+++ simple_steam_queries/__init__.py
@@ -0,0 +1,7 @@
+from .simple_steam_queries import SteamQueryParam as SteamQueryParam
+from .simple_steam_queries import Logical as Logical
+from .simple_steam_queries import SteamServerQuery as SteamServerQuery
+from .simple_steam_queries import sign_in as sign_in
+from .simple_steam_queries import get_server_list as get_server_list
+
+name = 'simple_steam_queries'
