32840b25281da23aae81c2541e1366f5618c675d
Giannis Spentzas <gspentzas1991@gmail.com>
2022-06-06 05:25:50 +0300

    adding package to pip

---
+++ __init__.py
@@ -0,0 +1,1 @@
+name = 'Simple-Steam-Queries'
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Simple-Steam-Queries",
+    install_requires=['steam'],
+    version="1.0.0",
+    author="Giannis Spentzas",
+    author_email="gspentzas1991@gmail.com",
+    description="A python module that allows you to easily run Steam Master Server Query Protocol queries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gspentzas1991/Simple-Steam-Queries",
+    packages=find_packages(include=['Simple-Steam-Queries', 'Simple-Steam-Queries.*']),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
